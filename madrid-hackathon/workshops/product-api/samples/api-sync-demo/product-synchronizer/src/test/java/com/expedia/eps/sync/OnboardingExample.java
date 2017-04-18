ba5c4dd32541805866cf1ecc9cd0ba065024a848
Rodrigo Hartmann <rgresellehartm@expedia.com>
2017-04-18 16:50:58 -0400

    Starting an onboarding example

--- madrid-hackathon/workshops/product-api/samples/api-sync-demo/product-synchronizer/src/test/java/com/expedia/eps/sync/OnboardingExample.java
+++ madrid-hackathon/workshops/product-api/samples/api-sync-demo/product-synchronizer/src/test/java/com/expedia/eps/sync/OnboardingExample.java
@@ -1,11 +1,12 @@
 package com.expedia.eps.sync;
 
-import static com.expedia.eps.property.model.StatusCodes.ONBOARDINGSUCCEEDED;
+import static java.util.Collections.singletonList;
 import static java.util.UUID.randomUUID;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.expedia.eps.ExpediaResponse;
+import com.expedia.eps.product.ProductApi;
 import com.expedia.eps.property.PropertyApi;
+import com.expedia.eps.property.model.Property;
 import com.expedia.eps.property.model.PropertyStatus;
 
 import org.junit.Test;
@@ -15,25 +16,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import lombok.extern.slf4j.Slf4j;
+import rx.Observable;
 
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class GetProperty {
+public class OnboardingExample {
 
     @Autowired
     private PropertyApi propertyApi;
 
+    @Autowired
+    private ProductApi productApi;
+
     @Test
-    public void getProperty() {
+    public void onboardProperty() throws Exception {
 
         final String requestId = randomUUID().toString();
+        final String providerId = "1000";
 
-        final PropertyStatus status = propertyApi.getPropertyStatus(requestId, "1000", "11275")
+        // TODO: build the object you are posting
+        Property property = Property.builder()
+            .build();
+
+        // Obtain the property id from Expedia
+        final Integer propertyId = propertyApi.createOrUpdateProperties(requestId, providerId, singletonList(property))
             .map(ExpediaResponse::getEntity)
+            .map(PropertyStatus::getExpediaId)
             .toBlocking()
             .single();
 
-        assertThat(status.getCode()).isEqualTo(ONBOARDINGSUCCEEDED);
+        // Now create rooms.. rates..
+
     }
 }
--- madrid-hackathon/workshops/product-api/samples/api-sync-demo/property-api-client/src/main/java/com/expedia/eps/property/PropertyApi.java
+++ madrid-hackathon/workshops/product-api/samples/api-sync-demo/property-api-client/src/main/java/com/expedia/eps/property/PropertyApi.java
@@ -1,7 +1,10 @@
 package com.expedia.eps.property;
 
 import com.expedia.eps.ExpediaResponse;
+import com.expedia.eps.property.model.Property;
 import com.expedia.eps.property.model.PropertyStatus;
+
+import java.util.List;
 
 import feign.Headers;
 import feign.Param;
@@ -10,7 +13,7 @@ import rx.Observable;
 
 public interface PropertyApi {
 
-//    @Headers({
+    //    @Headers({
 //        "Request-ID: {guid}",
 //        "TUID: 69107528",
 //        "Accept: application/json"
@@ -20,16 +23,15 @@ public interface PropertyApi {
 //                                                            @Param("provider") String provider,
 //                                                            @Param("providerPropertyId") String providerPropertyId);
 //
-//    @Headers({
-//        "Request-ID: {guid}",
-//        "TUID: 69107528",
-//        "Accept: application/json",
-//        "Content-Type: application/json"
-//    })
-//    @RequestLine("POST /v1/properties/{provider}")
-//    Observable<ExpediaResponse> createOrUpdateProperties(@Param("guid") String requestId,
-//                                                         @Param("provider") String provider,
-//                                                         List<Property> properties);
+    @Headers({
+        "Request-ID: {guid}",
+        "Accept: application/json",
+        "Content-Type: application/json"
+    })
+    @RequestLine("POST /v1/properties/{provider}")
+    Observable<ExpediaResponse<PropertyStatus>> createOrUpdateProperties(@Param("guid") String requestId,
+                                                                         @Param("provider") String provider,
+                                                                         List<Property> properties);
 //
 //    @Headers({
 //        "Request-ID: {guid}",
@@ -44,12 +46,10 @@ public interface PropertyApi {
 
     @Headers({
         "Request-ID: {guid}",
-        "TUID: {tuid}",
         "Accept: application/json"
     })
     @RequestLine("GET /v1/properties/{provider}/{providerPropertyId}/status")
     Observable<ExpediaResponse<PropertyStatus>> getPropertyStatus(@Param("guid") String requestId,
-                                                                  @Param("tuid") String tuid,
                                                                   @Param("provider") String provider,
                                                                   @Param("providerPropertyId") String providerPropertyId);
 }
